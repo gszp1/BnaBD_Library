cffec32a5a16e956d9e831fc1db22862f261e842
gszp1 <grzybekmateusz@poczta.fm>
2024-04-03 09:39:19 +0200

    Adds Genre entity class.

---
+++ src/main/java/org/example/library/model/Genre.java
@@ -0,0 +1,44 @@
+package org.example.library.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "genres")
+public class Genre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "genre_id")
+    private long id;
+
+    @Column(length = 255, nullable = false)
+    private String name;
+
+    public Genre() {
+        super();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Genre{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
